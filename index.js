984b21b63901c018fd425cae4451a01c6259ec84
unknown <sjxliu8@gmail.com>
2021-10-16 21:38:47 -0500

    character images for direction changes not working

--- index.js
+++ index.js
@@ -2,6 +2,80 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange (){
+
+if (direction === null){
+    character.src = "assets/green-character/static.gif"
+}
+if (direction === "west"){
+    character.src = "assets/green-character/west.gif"
+}
+if (direction === "east"){
+    character.src = "assets/green-character/east.gif"
+}
+if (direction === "south"){
+    character.src = "assets/green-character/south.gif"
+}
+if (direction === "north"){
+    character.src = "assets/green-character/north.gif"
+}
+}
+
+move(character).withArrowKeys(100,250, handleDirectionChange)
+
+// move(newImage("assets/tree.png")).withArrowKeys(200, 450)
+
+// let direction = null;
+// let x = 100;
+// let y = 250;
+
+// setInterval (function (){
+// if (direction === "west"){
+//     x = x - 1
+// }
+// if (direction === "east"){
+//     x = x + 1
+// }
+// if (direction === "north"){
+//     x = x + 1
+// }
+// if (direction === "south"){
+//     x = x - 1
+// }
+// character.style.left = x + "px"
+// character.style.bottom = y + "px"
+// }, 1 )
+
+function moveWithArrowKeys(left, bottom, callback){
+    let direction = null;
+    let x = left;
+    let y = bottom;
+}
+
+document.addEventListener("keydown", function(e){
+    if (e.repeat) return;
+    
+    if (e.key === "ArrowLeft"){
+        direction = "west"
+    }
+    if(e.key === "ArrowRight"){
+        direction = "east"
+    }
+    if (e.key === "ArrowUp"){
+        direction = "north"
+    }
+    if (e.key === "ArrowDown"){
+        direction = "south"
+    }
+}
+)
+
+document.addEventListener("keyup", function(e){
+    direction = null
+    callback(direction)
+})
+
 move(character).to(100, 250)
 
 
